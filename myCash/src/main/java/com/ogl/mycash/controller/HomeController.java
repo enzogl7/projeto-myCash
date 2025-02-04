3b1e35c054d006b505aac505642a30539f39dbee
Enzo Lima <enzolima527@gmail.com>
2025-02-04 11:46:30 -0300

    [main]

--- myCash/src/main/java/com/ogl/mycash/controller/HomeController.java
+++ myCash/src/main/java/com/ogl/mycash/controller/HomeController.java
@@ -1,10 +1,17 @@
 package com.ogl.mycash.controller;
 
+import com.ogl.mycash.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("/home")
     public String home() {
         return "/home/home_primeiro_login";
@@ -13,5 +20,11 @@ public class HomeController {
     @GetMapping("/dashboard")
     public String dashboard() {
         return "/home/home";
+    }
+
+    @GetMapping("/adicionar-receita")
+    public String adicionarReceita(Model model) {
+        model.addAttribute("moedaPrincipal", usuarioService.getMoedaPrincipalByUsuario(usuarioService.getUsuarioLogado()));
+        return "/receita/adicionar_receita";
     }
 }
--- myCash/src/main/java/com/ogl/mycash/service/UsuarioService.java
+++ myCash/src/main/java/com/ogl/mycash/service/UsuarioService.java
@@ -1,8 +1,12 @@
 package com.ogl.mycash.service;
 
+import com.ogl.mycash.infra.AuthenticationFacade;
 import com.ogl.mycash.model.Usuario;
 import com.ogl.mycash.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +14,20 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private AuthenticationFacade authenticationFacade;
+
     public void salvar(Usuario usuario) {
         usuarioRepository.save(usuario);
+    }
+
+    public Usuario getUsuarioLogado() {
+        String emailUsuario = authenticationFacade.getAuthentication().getName();
+        return usuarioRepository.findByEmail(emailUsuario)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + emailUsuario));
+    }
+
+    public String getMoedaPrincipalByUsuario(Usuario usuario) {
+        return usuario.getMoedaPreferida();
     }
 }
